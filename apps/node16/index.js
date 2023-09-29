4ca2517033514b2e29b1f98d9f1c9c31b32b40f1
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2023-09-29 15:31:17 +0200

    Initial commit

---
+++ apps/node16/index.js
@@ -0,0 +1,5 @@
+const { printNode16PackageVersion } = require('node16Dep');
+
+console.log('App Node16 running version: ' + process.version);
+
+printNode16PackageVersion();
---
+++ packages/node16Dep/index.js
@@ -0,0 +1,7 @@
+const printNode16PackageVersion = () => {
+  console.log('Node16Dep running version: ' + process.version);
+};
+
+module.exports = {
+  printNode16PackageVersion
+};
