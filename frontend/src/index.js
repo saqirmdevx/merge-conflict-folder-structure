eec9c5788aabc7a64a6f185d38a91ae78323012d
Saqirmdevx <martin.nascak@student.tuke.sk>
2023-10-01 20:29:04 +0200

    Update frontend code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,8 @@
 const { printNode16PackageVersion } = require('node16Dep');
-console.log('App Node18 running version: ' + process.version);
+console.log('Version of frontend: ' + process.version);
 
 printNode16PackageVersion();
+
+// Calculate the sum of two numbers
+const sum = (a, b) => a + b;
+console.log(sum(5, 10));
