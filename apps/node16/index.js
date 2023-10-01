adfe1051d92905767c1b5ad0c86710c47316e54c
Saqirmdevx <martin.nascak@student.tuke.sk>
2023-10-01 20:24:33 +0200

    Add files

--- apps/node16/index.js
+++
@@ -1,5 +0,0 @@
-const { printNode16PackageVersion } = require('node16Dep');
-
-console.log('App Node16 running version: ' + process.version);
-
-printNode16PackageVersion();
--- apps/node18/index.js
+++
@@ -1,4 +0,0 @@
-const { printNode16PackageVersion } = require('node16Dep');
-console.log('App Node18 running version: ' + process.version);
-
-printNode16PackageVersion();
